50730bb1be11a09837633e3b1439a9a8ed89c49b
Karanpong2211310079 <tankangamer@gmail.com>
2025-05-24 16:06:26 +0700

    refactor server setup: improve middleware organization and add root status route

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json()) // << สำคัญมาก ต้องมี!
-// Set the port
-const PORT = process.env.PORT || 3000;
+app.use(express.json()); // สำคัญมาก ต้องมี!
+
 // Set headers for all responses
 app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+// Route root แสดงสถานะเซิร์ฟเวอร์
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is running' });
 });
 
 require("./routes/login.routes")(app);
 require("./routes/taro.routes")(app);
 require("./routes/user.routes")(app);
 require("./routes/redeem.routes")(app);
+
+// Set the port
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
