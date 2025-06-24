8c2287604290c3a3116885dc793ba562883b9841
Karanpong2211310079 <tankangamer@gmail.com>
2025-06-24 20:11:27 +0700

    enhance login controller: update login logic to use phone number instead of password

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -3,16 +3,16 @@ const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
 })
 exports.login = async (req, res) => {
-  const { name, password } = req.body;
+  const { name, phone } = req.body;
 
   try {
-    if (!name || !password) {
+    if (!name || !phone) {
       return res.status(400).send({ message: 'Name and phone are required.' });
     }
 
     // หา user ที่มีเบอร์นี้
     const existingUserByPhone = await prisma.users.findFirst({
-      where: { phone_number: password },
+      where: { phone_number: phone },
     });
 
     if (existingUserByPhone) {
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
     const newUser = await prisma.users.create({
       data: {
         name: name,
-        phone_number: password,
+        phone_number: phone,
       },
     });
 
